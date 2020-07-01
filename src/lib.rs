188c80c752c80886e37f07ed6acdc21e370e1e63
floatingmountain <shattered.web.master@gmail.com>
2020-07-01 16:57:41 +0200

    Identifier

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,26 @@ mod imagedata;
 
 pub use fileloader::AsyncFileLoader;
 pub use filemanager::AsyncFileManager;
+use std::path::PathBuf;
 ///
 #[derive(Debug, Eq, PartialEq)]
 pub enum LoadStatus {
     NotLoading,
     Loading,
     Loaded,
+}
+#[derive(Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
+pub enum Identifier{
+    Path(PathBuf),
+    Index(usize),
+}
+impl From<PathBuf> for Identifier{
+    fn from(p: PathBuf) -> Self {
+        Identifier::Path(p)
+    }
+}
+impl From<usize> for Identifier{
+    fn from(u: usize) -> Self {
+        Identifier::Index(u)
+    }
 }
